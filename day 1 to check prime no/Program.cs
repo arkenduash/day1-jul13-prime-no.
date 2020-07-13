167588758999cfbf4ddcc92ac389f2eabb144bcd
unknown <arkendu98@gmail.com>
2020-07-13 15:51:47 +0530

    initial commit

---
+++ day 1 to check prime no/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_1_to_check_prime_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5, a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
